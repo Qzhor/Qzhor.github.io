eca3defae2da2de5899657aa942de8870deba7bc
joki <694057567@qq.com>
2018-12-09 21:57:08 +0800

    update_layer_2_vue

--- js/login.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-layui.use(['jquery'],function(){
-	window.jQuery = window.$ = layui.jquery;
-	$(".layui-canvs").width($(window).width());
-	$(".layui-canvs").height($(window).height());
-
-});
